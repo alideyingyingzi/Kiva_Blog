89109468b238db1d132d9c0ad18da4ce21dbd293
alideyingyingzi <912258330@qq.com>
2026-03-18 19:36:39 +0800

    shanchu

--- src/features/theme/contract/pages/home.ts
+++
@@ -1,5 +0,0 @@
-import type { PostItem } from "@/features/posts/schema/posts.schema";
-
-export interface HomePageProps {
-  posts: Array<PostItem>;
-}
--- src/lib/db/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./auth.table";
-export * from "./comments.table";
-export * from "./config.table";
-export * from "./friend-links.table";
-export * from "./media.table";
-export * from "./post-revisions.table";
-export * from "./posts.table";
